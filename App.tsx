76763967d52b79cdc65fad1d7310370a65297ada
Gizem Gulec <gizem.gulec@dominospizza.com.tr>
2023-07-01 22:29:18 +0300

    adding some styles and managing buttons activation

--- App.tsx
+++ App.tsx
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
-  StatusBar,
   StyleSheet,
-  Text,
   View,
+  ImageBackground,
+  SafeAreaView
 } from 'react-native';
-
 import StartGameScreen from './screens/StartGameScreen';
+import LinearGradient from 'react-native-linear-gradient';
+import GameScreen from './screens/GameScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  function pickedNumberHandler (pickedNumber) {
+    setUserNumber(pickedNumber);
+  } 
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>
+
+  if (userNumber) {
+    screen = <GameScreen/>
+  }
+
+
   return (
-    <View>
-      <StartGameScreen />
+    <View style={styles.rootScreen}>
+      <ImageBackground 
+        source={require('./assets/images/dices.png')} 
+        resizeMode='cover'
+        style={styles.rootScreen}
+        imageStyle={styles.dicesImage}
+      >
+        {/* <StartGameScreen onPickNumber={pickedNumberHandler}/> */}
+        <SafeAreaView style={styles.rootScreen}>
+          {screen}
+        </SafeAreaView>
+      </ImageBackground>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex :1,
+    //opacity: 0.20,
+    //backgroundColor: '#ddb52f'
+  },
+  dicesImage: {
+    opacity: 0.20,
+    backgroundColor: '#ddb52f'
+  }
+});
